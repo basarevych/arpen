640058ab8523b651127d5d7bdb0663ed8e5a8b68
Ross Basarevych <basarevych@gmail.com>
2017-03-01 16:48:56 +0200

    Load all commands

--- src/app/console.js
+++ src/app/console.js
@@ -25,20 +25,17 @@ class Console extends App {
                     process.exit(1);
                 }
 
-                return this.constructor._require(path.join(config.base_path, 'commands', this.argv['_'][0]))
-                    .catch(error => {
-                        console.error('No such command');
-                        process.exit(1);
-                    });
-            })
-            .then(obj => {
-                if (!obj || !obj.provides)
-                    throw new Error('Command does not returned a class');
+                let command, util = this.get('util');
+                try {
+                    command = this.get(`commands.${util.dashedToCamel(this.argv['_'][0])}`);
+                } catch (error) {
+                    console.error('Unknown command');
+                    process.exit(1);
+                }
 
                 this._running = true;
 
-                this.registerClass(obj);
-                let result = this.get(obj.provides).run(this.argv);
+                let result = command.run(this.argv);
                 if (result === null || typeof result != 'object' || typeof result.then != 'function')
                     throw new Error(`Command '${this.argv['_'][0]}' run() did not return a Promise`);
                 return result;
