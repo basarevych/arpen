d853efe2a31551f6647481163ad7266edaaa7a36
Ross Basarevych <basarevych@gmail.com>
2017-09-29 11:36:57 +0300

    Better search and version bump

--- src/repositories/mysql/search.js
+++ src/repositories/mysql/search.js
@@ -11,13 +11,14 @@ const NError = require('nerror');
  * @method search
  * @memberOf module:arpen/repositories/mysql~MySQLRepository
  * @param {object} [options]
- * @param {string} [options.table]               Table or view name
- * @param {string[]} [options.fields]            Fields to retrieve
- * @param {string[]} [options.where]             SQL WHERE clause: will be joined with 'AND'
- * @param {Array} [options.params]               Bound parameters of WHERE (referenced as $1, $2, ... in SQL)
- * @param {string} [options.sort=['id asc']]     Used in ORDER BY if provided
- * @param {number} [options.pageSize=0]          Used in LIMIT, 0 = all records
- * @param {number} [options.pageNumber=1]        Used in OFFSET
+ * @param {string} [options.table]              Table or view name
+ * @param {string[]} [options.fields]           Fields to retrieve
+ * @param {string[]} [options.where]            SQL WHERE clause: will be joined with 'AND'
+ * @param {Array} [options.params]              Bound parameters of WHERE (referenced as $1, $2, ... in SQL)
+ * @param {string} [options.sort=['id asc']]    Used in ORDER BY if provided
+ * @param {number} [options.pageSize=0]         Used in LIMIT, 0 = all records
+ * @param {number} [options.pageNumber=1]       Used in OFFSET
+ * @param {boolean} [options.infoOnly=false]    Retrieve data rows or not
  * @param {MySQLClient|string} [mysql]          Will reuse the MySQL client provided, or if string then will
  *                                              connect to this instance of MySQL.
  * @return {Promise}                            Returns promise resolving to the following:
@@ -38,9 +39,10 @@ module.exports = async function (options = {}, mysql = undefined) {
         fields = Object.keys(this.getModel()._serialize()),
         where = [],
         params = [],
-        sort = [ 'id asc' ],
+        sort = ['id asc'],
         pageSize = 0,
-        pageNumber = 1
+        pageNumber = 1,
+        infoOnly = false,
     } = options;
 
     let client;
@@ -50,7 +52,7 @@ module.exports = async function (options = {}, mysql = undefined) {
         let result = await client.query(
             `SELECT count(*) AS count 
                FROM ${table} 
-             ${where.length ? `WHERE (${where.join(') AND (')})` : ''}`,
+                    ${where.length ? `WHERE (${where.join(') AND (')})` : ''}`,
             params
         );
 
@@ -65,16 +67,18 @@ module.exports = async function (options = {}, mysql = undefined) {
                 pageNumber = totalPages;
         }
 
-        let offset = (pageNumber - 1) * pageSize;
-        result = await client.query(
-            `SELECT ${fields.join(', ')}
-               FROM ${table} 
-             ${where.length ? `WHERE (${where.join(') AND (')})` : ''},
-             ${sort.length ? `ORDER BY ${sort.join(', ')}` : ''}
-             ${offset > 0 ? `OFFSET ${offset}` : ''}
-             ${pageSize > 0 ? `LIMIT ${pageSize}` : ''}`,
-            params
-        );
+        if (!infoOnly) {
+            let offset = (pageNumber - 1) * pageSize;
+            result = await client.query(
+                `SELECT ${fields.join(', ')}
+                   FROM ${table} 
+                        ${where.length ? `WHERE (${where.join(') AND (')})` : ''}
+                        ${sort.length ? `ORDER BY ${sort.join(', ')}` : ''}
+                        ${pageSize > 0 ? `LIMIT ${pageSize}` : ''}
+                        ${offset > 0 ? `OFFSET ${offset}` : ''}`,
+                params
+            );
+        }
 
         if (typeof mysql !== 'object')
             client.done();
@@ -85,7 +89,7 @@ module.exports = async function (options = {}, mysql = undefined) {
             pageSize: pageSize,
             pageNumber: pageNumber,
             sort: sort,
-            data: result,
+            data: infoOnly ? [] : result,
         };
     } catch (error) {
         if (client && typeof mysql !== 'object')
--- src/repositories/postgres/search.js
+++ src/repositories/postgres/search.js
@@ -11,13 +11,14 @@ const NError = require('nerror');
  * @method search
  * @memberOf module:arpen/repositories/postgres~PostgresRepository
  * @param {object} [options]
- * @param {string} [options.table]               Table or view name
- * @param {string[]} [options.fields]            Fields to retrieve
- * @param {string[]} [options.where]             SQL WHERE clause: will be joined with 'AND'
- * @param {Array} [options.params]               Bound parameters of WHERE (referenced as $1, $2, ... in SQL)
- * @param {string} [options.sort=['id asc']]     Used in ORDER BY if provided
- * @param {number} [options.pageSize=0]          Used in LIMIT, 0 = all records
- * @param {number} [options.pageNumber=1]        Used in OFFSET
+ * @param {string} [options.table]              Table or view name
+ * @param {string[]} [options.fields]           Fields to retrieve
+ * @param {string[]} [options.where]            SQL WHERE clause: will be joined with 'AND'
+ * @param {Array} [options.params]              Bound parameters of WHERE (referenced as $1, $2, ... in SQL)
+ * @param {string} [options.sort=['id asc']]    Used in ORDER BY if provided
+ * @param {number} [options.pageSize=0]         Used in LIMIT, 0 = all records
+ * @param {number} [options.pageNumber=1]       Used in OFFSET
+ * @param {boolean} [options.infoOnly=false]    Retrieve data rows or not
  * @param {PostgresClient|string} [pg]          Will reuse the Postgres client provided, or if string then will
  *                                              connect to this instance of Postgres.
  * @return {Promise}                            Returns promise resolving to the following:
@@ -40,7 +41,8 @@ module.exports = async function (options = {}, pg = undefined) {
         params = [],
         sort = [ 'id asc' ],
         pageSize = 0,
-        pageNumber = 1
+        pageNumber = 1,
+        infoOnly = false,
     } = options;
 
     let client;
@@ -65,16 +67,18 @@ module.exports = async function (options = {}, pg = undefined) {
                 pageNumber = totalPages;
         }
 
-        let offset = (pageNumber - 1) * pageSize;
-        result = await client.query(
-            `SELECT ${fields.join(', ')}
-               FROM ${table} 
-             ${where.length ? `WHERE (${where.join(') AND (')})` : ''},
-             ${sort.length ? `ORDER BY ${sort.join(', ')}` : ''}
-             ${offset > 0 ? `OFFSET ${offset}` : ''}
-             ${pageSize > 0 ? `LIMIT ${pageSize}` : ''}`,
-            params
-        );
+        if (!infoOnly) {
+            let offset = (pageNumber - 1) * pageSize;
+            result = await client.query(
+                `SELECT ${fields.join(', ')}
+                   FROM ${table} 
+                        ${where.length ? `WHERE (${where.join(') AND (')})` : ''}
+                        ${sort.length ? `ORDER BY ${sort.join(', ')}` : ''}
+                        ${offset > 0 ? `OFFSET ${offset}` : ''}
+                        ${pageSize > 0 ? `LIMIT ${pageSize}` : ''}`,
+                params
+            );
+        }
 
         if (typeof pg !== 'object')
             client.done();
@@ -85,7 +89,7 @@ module.exports = async function (options = {}, pg = undefined) {
             pageSize: pageSize,
             pageNumber: pageNumber,
             sort: sort,
-            data: result.rows,
+            data: infoOnly ? [] : result.rows,
         };
     } catch (error) {
         if (client && typeof pg !== 'object')
